7ed03f46cdf8db8ea9a9f06027c645933c5e3995
akoes <abhishekkansal97@gmail.com>
2018-03-06 03:43:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ function createTemplate(data){
         
     </html>
     `;
-    return htmlTemaplate;
+    return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
