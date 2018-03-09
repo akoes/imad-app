d1cc5900764df58072af7fa9f88676b12bac5b54
akoes <abhishekkansal97@gmail.com>
2018-03-10 01:39:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',fumction(req,res){
+    res.send('Article one requested and will be served here');
+});
+
+app.get('/article-two',fumction(req,res){
+    res.send('Article one requested and will be served here');
+});
+
+app.get('/article-three',fumction(req,res){
+    res.send('Article one requested and will be served here');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
