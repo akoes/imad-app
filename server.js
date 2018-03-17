b0e31f61e383cac81a910dca01e9fcc30c637758
akoes <abhishekkansal97@gmail.com>
2018-03-17 23:44:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var Pool = require('pg').Pool;
 
 var config = {
     user: 'abhishekkansal97',
-    database: 'db.imad.hasura-app.io',
+    database: 'abhishekkansal97',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
 };
