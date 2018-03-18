0e027b70cb3741ca3ef715f8f21a9f8d20f5b5b3
akoes <abhishekkansal97@gmail.com>
2018-03-18 12:32:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,8 +98,9 @@ app.get('/articles/:articleName', function (req, res) {
                 res.status(404).send('Article not found');
             }
             else {
+                console.log(result.rows[0]);
                 var articleData = result.rows[0];
-                res.send(console.log(createTemplate(articleData)));
+                res.send(createTemplate(articleData));
             }
         }
         
